1f59b2f27d967f1c6e81527dc7a57c17fd0a1e06
proyectoanaclara <126722916+proyectoanaclara@users.noreply.github.com>
2023-03-08 15:52:38 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+ 
+// Tuve la intención de poner un botón VER VIDEO y que al 
+// presionarlo se mostrara el video oculto, lo intente de varias 
+// maneras pero no pude lograrlo.
+
+// const btn = document.querySelector("button");
+// const box = document.querySelector("div");
+// const video = document.querySelector("video");
+
+// btn.addEventListener("click", () => box.classList.remove("hidden"));
+// video.addEventListener("click", () => video.play());
+// box.addEventListener("click", () => box.classList.add("hidden"));
